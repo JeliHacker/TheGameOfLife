7c5bf27436f5dc7887f2ab6190dca691a02bf550
JeliHacker <jeligooch@gmail.com>
2022-09-20 23:11:38 +0200

    section 3: interact with the grid

--- code.js
+++ code.js
@@ -1,9 +1,12 @@
 var rows = 24;
 var cols = 24;
 
+var playing = false;
+
 // initialize
 function initialize() {
     createTable();
+    setupControlButtons();
 }
 
 // lay out the board
@@ -21,11 +24,52 @@ function createTable() {
             var cell = document.createElement("td");
             cell.setAttribute("id", i + "_" + j);
             cell.setAttribute("class", "dead");
+            cell.onclick = cellClickHandler;
             tr.appendChild(cell);
         }
         table.appendChild(tr);
     }
     gridContainer.appendChild(table);
+}
+
+function cellClickHandler() {
+    var classes = this.getAttribute("class");
+    if (classes.indexOf("live") > -1) {
+        this.setAttribute("class", "dead");
+    } else {
+        this.setAttribute("class", "live");
+    }
+    classes = this.getAttribute("class")
+}
+
+function setupControlButtons() {
+    // button to start
+    var startButton = document.getElementById("start");
+    startButton.onclick = startButtonHandler;
+
+    // button to clear
+    var clearButton = document.getElementById("clear");
+    clearButton.onclick = clearButtonHandler;
+}
+
+function clearButtonHandler() {
+    console.log("Clear the game: stop playing, clear the grid");
+    playing = false;
+    var startButton = document.getElementById("start");
+    startButton.innerHTML = "start";
+}
+
+function startButtonHandler() {
+    if (playing) {
+        console.log("Pause the game");
+        playing = false;
+        this.innerHTML = "continue";
+    } else {
+        console.log("Continue the game");
+        playing = true;
+        this.innerHTML = "pause";
+        play();
+    }
 }
 
 // start everything
